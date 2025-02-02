2836a604cad71b13d2e39ab2e727f8f3b5babbca
JinHa Park <160022452+jjiinaaa@users.noreply.github.com>
2025-02-03 00:17:40 +0900

    [CARE-40] 비상연락망 컴포넌트 레이아웃 구조 개선

--- src/components/myInfo/FamilyInfoBox.tsx
+++ src/components/myInfo/FamilyInfoBox.tsx
@@ -5,8 +5,8 @@ import { FamilyInfo } from '@_types/familyInfo';
 
 function FamilyInfoBox({ name, relationship, phoneNumber }: FamilyInfo): React.JSX.Element {
   return (
-    <View className="flex-row rounded-lg py-4 px-2 w-full">
-      <View className="flex-row items-baseline">
+   <View className="flex-row rounded-lg py-4 px-2 w-full justify-between"> 
+     <View className="flex-row items-baseline space-x-2">
         <Text className=" text-lg font-bold text-gray100" numberOfLines={1} ellipsizeMode="clip">
           {name}
         </Text>
@@ -14,7 +14,7 @@ function FamilyInfoBox({ name, relationship, phoneNumber }: FamilyInfo): React.J
           {relationship}
         </Text>
       </View>
-      <View className=" flex-row justify-center">
+      <View className=" flex-row">
         <Text className="text-lg text-center text-gray100">{phoneNumber}</Text>
       </View>
     </View>
