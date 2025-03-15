d9557e35eb91b227533286d71be07a4f674df096
jpark0506 <peterp1@inha.edu>
2025-03-15 18:13:58 +0900

    [CARE-61] chore : 코드 정리

--- App.tsx
+++ App.tsx
@@ -16,10 +16,7 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Main"
-          component={MainScreen}
-        />
+        <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Test" component={TestScreen} />
         <Stack.Screen name="SampleHome" component={SampleHomeScreen} />
